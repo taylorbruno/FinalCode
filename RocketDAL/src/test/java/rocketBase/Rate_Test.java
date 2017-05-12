1fdb5ce3d0470e802f2ebdf28d357106be243425
Taylor Bruno <taytay627@wifi-roaming-128-4-87-4.host.udel.edu>
2017-05-11 22:04:55 -0400

    c4

--- RocketDAL/src/test/java/rocketBase/Rate_Test.java
+++ RocketDAL/src/test/java/rocketBase/Rate_Test.java
@@ -3,20 +3,25 @@ package rocketBase;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-
 import org.junit.Test;
-
 import rocketDomain.RateDomainModel;
 
 public class Rate_Test {
 
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a known credit score returns a known interest rate
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a RateException is thrown if there are no rates for a given
-	//		credit score
 
+	@Test
+	public void test() {
+		
+		ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
+		System.out.println ("Rate Size:" + rates.size());
+		assert(rates.size() > 0);
+		assert(1==1);
+	}
 
+	//Check that rates in tblRate are >0
+	@Test
+	public void testGetAllRates() {
+		ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
+		assertTrue(rates.size() > 0);
+	}
 }
